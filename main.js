83b6d931210b7956cb1599d2f83138c39ccd31a3
AksshayaSivakumar <aksshayapugal@gmail.com>
2023-09-07 17:33:38 +0530

    deleting_data_from_local_storage_new

--- main.js
+++ main.js
@@ -1,47 +1,41 @@
 const myForm = document.getElementById('my-form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
-const itemlist=document.getElementById('users')
+//const itemlist=document.getElementById('users')
+
 
 myForm.addEventListener('submit', onSubmit);
-itemlist.addEventListener('click',removeitem);
-
   
 function onSubmit(e)
 {
   e.preventDefault();
-  var name=JSON.stringify(nameInput.value);
-  var email=JSON.stringify(emailInput.value);
+ 
+    const name=nameInput.value
+    const email=emailInput.value
   
-  localStorage.setItem(name, email);
+    let obj={
+      name,
+      email
+    }
   
+  localStorage.setItem(obj.email,JSON.stringify(obj));
+ showonscreen(obj);
+  }
 //  var name1=JSON.parse(name);
 //  var email1=JSON.parse(email);
-  var li=document.createElement('li')
-  li.className='list-group-item'
-  
-
-
-li.appendChild(document.createTextNode(name))
-li.appendChild(document.createTextNode(" - "))
-li.appendChild(document.createTextNode(email))
-
-var deleteBtn=document.createElement('button')
-
-deleteBtn.className='btn btn-danger btn-sm float-right delete mr-3'
-deleteBtn.appendChild(document.createTextNode('Delete'))
-li.appendChild(deleteBtn);
-itemlist.appendChild(li)
-}
-
-function removeitem(e)
+function showonscreen(obj)
 {
-  
-  if(e.target.classList.contains('delete'))
-  {
-var li=e.target.parentElement
-itemlist.removeChild(li);
-localStorage.removeItem(JSON.stringify(nameInput.value));
-
+  const parentElem =document.getElementById('users')
+  const childElem=document.createElement('li')
+  childElem.textContent=obj.name+ '-' +obj.email
+  const deleteBtn=document.createElement('input')
+  deleteBtn.type='button'
+  deleteBtn.value='delete'
+  deleteBtn.onclick=()=>{
+    localStorage.removeItem(obj.email)
+    parentElem.removeChild(childElem)
   }
+  childElem.appendChild(deleteBtn)
+  parentElem.appendChild(childElem)
 }
+  
